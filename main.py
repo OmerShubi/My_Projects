c01b436b9fa7fec88e5ad0f28702615e8c9b76c6
scaperex <omer.shubi@gmail.com>
2019-06-18 22:08:32 +0300

    WIP

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ if __name__ == '__main__':
     file_name = './movie_metadata.csv'
 
     # TODO KNN (K=10) 5-fold Cross Validation
+    # sklearn.neighbors.KNeighborsClassifier
     # TODO Rocchio 5-fold Cross Validation
+    # sklearn.neighbors.NearestCentroid
     # TODO for each - Precision, Recall, Accuracy
     # TODO PRINT RESULTS
     pass
