2d7668f1f95ede5b3e00fa38f0adb7f6c87bb6e1
scaperex <omer.shubi@gmail.com>
2019-06-19 11:25:24 +0300

    WIP

--- Data.py
+++ Data.py
@@ -1,33 +1,34 @@
-from sklearn import preprocessing
+import sklearn
+import pandas as pd
 import numpy as np
 
 # Reads the csv file and save data to memory
 class Data:
     def __init__(self, file_name):
-        self.file = file_name
+        self.file = file_name # path to file
+        self.data = pd.array
 
     def preprocess(self):
-        doc_set = {}
-        index = 0
-        with open(self.file, 'r') as reader:
-            for line in reader:
-                vec = 26*[0,]
-                for word in line.split("\t")[0].split():
-                    word = self.pre_process_word(word)
-                    if word == '':
-                        continue
-                    vec[self.words[word]] = 1
-                doc_class = line.split("\t")[1].rstrip()
-                vec.append(doc_class)
-                doc_set['doc'+str(index)] = vec
-                index += 1
-        return doc_set
-        pass
-        # TODO with file...
-        # TODO Preprocess function member - save all Attributes excluding content_Rating, movie_imdb_link, plot_keywords
-        # TODO discard entries with any NaN value
-        # TODO Handle duplicate movie_tile values
-        # TODO handle Categorical Attributes - turn to dummy variables sklearn.preprocessing.OneHotEncoder
+        # import csv
+        scaler = sklearn.preprocessing.StandardScaler()
+
+        self.data = pd.read_csv(self.file, delimiter=',')
+
+        # save all Attributes excluding content_Rating, movie_imdb_link, plot_keywords
+        self.data.drop(columns=['content_rating', 'movie_imdb_link', 'plot_keywords'], inplace=True)
+
+        # discard entries with any NaN value
+        self.data.dropna(inplace=True)
+
+        #  Handle duplicate movie_tile values
+        self.data.drop_duplicates(subset='movie_title', inplace=True)
+
+        #  handle Categorical Attributes - turn to dummy variables
+        self.data = pd.get_dummies(self.data)
+
+
+        # self.X_train, self.X_test, self.y_train, self.y_test = \
+        # train_test_split_split(X, y, random_state=0)
         # TODO Normalize - (x-mean)/SD - all variables except Categorical Attributes (dummy variables) sklearn.preprocessing.StandardScaler
 
     def splitToFiveFolds(self):
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-import sys
+from sklearn.datasets import load_digits
+import pandas as pd
+import Data
+import sklearn
+
+import AlgorithmRunner
+
+
 if __name__ == '__main__':
     file_name = './movie_metadata.csv'
 
+    data = Data.Data(file_name)
+    data = data.preprocess()
     # TODO KNN (K=10) 5-fold Cross Validation
     # sklearn.neighbors.KNeighborsClassifier
     # TODO Rocchio 5-fold Cross Validation
     # sklearn.neighbors.NearestCentroid
     # TODO for each - Precision, Recall, Accuracy
-    # TODO PRINT RESULTS
-    pass
+
+    print("Question 1:")
+    # print("KNN classifier", KNN_precision, KNN_recall, KNN_accuracy)
+    # print("Rocchio classifier:", Rocchio_precision, Rocchio_recall, Rocchio_accuracy)
+
+    print("Question 2:")
+    # print("KNN classifier", KNN_improved_accuracy)
+    # print("Rocchio classifier:", Rocchio_improved_accuracy)
+
+
