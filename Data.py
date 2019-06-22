ef663fdeccf8e571dd7dbd02bfb1474b722e4443
scaperex <omer.shubi@gmail.com>
2019-06-23 01:17:28 +0300

    actor merge and onehot - slow

--- Data.py
+++ Data.py
@@ -24,20 +24,34 @@ class Data:
         #  Handle duplicate movie_tile values
         data.drop_duplicates(subset='movie_title', keep='first', inplace=True)
 
-        self.y = data['imdb_score']
-        data = data.drop(columns=['imdb_score'], axis=1)
-        genres = data.pop('genres').str.get_dummies('|')
+        # As movie title is now unique we can discard it
+        data.drop(columns=['movie_title'], inplace=True)
 
+        # saves imdb score as labels
+        self.y = data['imdb_score']
+
+        # Discard label from data
+        data = data.drop(columns=['imdb_score'], axis=1)
+        print(data.head())
+
+        genres = data.pop('genres').str.get_dummies()
+        # actors = data.pop('actor_1_name')+"|"+data.pop('actor_2_name')+"|"+data.pop('actor_3_name')
+        # print(actors.head())
+
+        # actors = actors.str.get_dummies()
         numerical_columns = data.select_dtypes(include='number').columns
         categorical_columns = data.select_dtypes(exclude='number').columns
+        # print(actors.head())
         data = data.join(genres)
-
+        # data = data.join(actors)
         preprocessor = compose.ColumnTransformer(transformers=
                                                  [('num', preprocessing.StandardScaler(), numerical_columns),
                                                   ('cat', preprocessing.OneHotEncoder(), categorical_columns)],
                                                  remainder='passthrough')
 
         self.X = preprocessor.fit_transform(data)
+        print(self.X.shape)
+
         self.y = preprocessing.Binarizer(6.95).fit_transform(self.y.to_numpy().reshape(-1, 1))
         self.y = ravel(self.y)
 
