bfd6820f288c0f101107a4424c6255de90a1dfa2
scaperex <omer.shubi@gmail.com>
2019-06-26 09:54:30 +0300

    Back to basic

--- Data.py
+++ Data.py
@@ -1,13 +1,13 @@
 from sklearn import preprocessing, compose, model_selection
 from pandas import read_csv, DataFrame, Series
-from numpy import ravel, savetxt
+from numpy import ravel, savetxt, concatenate
 from scipy import sparse
 
 
 # Reads the csv file and save data to memory
 class Data:
     def __init__(self, file_name):
-        self.file = file_name  # path to file
+        self.file = file_name #  path to file
         self.X = DataFrame  # Data without Labels
         self.y = DataFrame  # Labels
 
@@ -28,62 +28,33 @@ class Data:
         # As movie title is now unique we can discard it
         data.drop(columns=['movie_title'], inplace=True)
 
-        # saves imdb score as labels
-        self.y = data['imdb_score']
-
-        # Discard label from data
-        data.drop(columns=['imdb_score'], axis=1, inplace=True)
+        # saves imdb score as labels & Discard label from data
+        self.y = data.pop('imdb_score')
 
         # Turn into dummy variables and discard original column from data
         genres = data.pop('genres').str.get_dummies()
+        # Merge the 3 actors into one column & delete original columns from data & Turn into dummy variables
+        actors = (data.pop('actor_1_name')+"|"+data.pop('actor_2_name')+"|"+data.pop('actor_3_name')).str.get_dummies()
 
-    #######
-        # actor1 = data['actor_1_name']
-        # actor2 = data['actor_2_name']
-        # actor3 = data['actor_3_name']
-        # actors = actor1.str.cat(actor2, sep="|")
-        # actors = actors.str.cat(actor3, sep="|")
-        #
-        # print(actors.head())
-        # print("1",genres.head())
-        #
-        # actors = actors.str.get_dummies('|')
-        # print("2", actors.head())
-        #
-        # data.drop(columns=['actor_1_name','actor_2_name','actor_3_name'], axis=1, inplace=True)
-#####
-        # Merge the 3 actors into one column & delete original columns from data
-        # actors = data.pop('actor_1_name')+"|"+data.pop('actor_2_name')+"|"+data.pop('actor_3_name')
-
-        # Turn into dummy variables
-        # actors = actors.str.get_dummies()
-
-
+        # Create column lists for transformer
         numerical_columns = data.select_dtypes(include='number').columns
         categorical_columns = data.select_dtypes(exclude='number').columns
-        categorical_columns = categorical_columns.drop(["actor_1_name", "actor_2_name", "actor_3_name"])
-
-
-        print("got here")
-        # data["actors"] = data.join(str.cat((data.pop("actor_1_name"),data.pop["actor_2_name"]), sep="|"))
-        data = data.join(data.pop('actor_1_name').str.cat(data.pop('actor_2_name'), sep="|"))
-        data = data.join(data.pop('actor_1_name').str.cat(data.pop("actor_3_name"), sep="|"))
-        data = data.join(data.pop("actor_1_name").str.get_dummies())
-        print("and here")
-
 
         data = data.join(genres)
-#         data.to_csv("features.csv")
+        data = data.join(actors)
+
         preprocessor = compose.ColumnTransformer(transformers=
                                                  [('num', preprocessing.StandardScaler(), numerical_columns),
                                                   ('cat', preprocessing.OneHotEncoder(), categorical_columns)],
-                                                 remainder='passthrough')
+                                                 remainder="passthrough")
 
         self.X = preprocessor.fit_transform(data)
-
+        # actors = actors.to_sparse()
+        # genres = genres.to_sparse()
+        # sel.X
         self.y = preprocessing.Binarizer(6.95).fit_transform(self.y.to_numpy().reshape(-1, 1))
         self.y = ravel(self.y)
-        # savetxt('labels.csv', self.y)
+
     @staticmethod
     def splitToFiveFolds():
         return model_selection.KFold(n_splits=5, shuffle=False, random_state=1)
