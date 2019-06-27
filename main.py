8f1d881988f2a0c47a5c6b85a403e8e6fa115b28
scaperex <omer.shubi@gmail.com>
2019-06-27 13:19:49 +0300

    tests in progress

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def part2(file_name):
     # Initialize and run KNN classifier
     print("Running KNN Classifier...")
     results =[]
-    for k in range(1, 20, 2):
+    for k in range(7, 20, 2):
 
         classifier_knn = AlgorithmRunner('KNN', number_of_neighbors=k)
         classifier_knn.cross_val_score(data)
