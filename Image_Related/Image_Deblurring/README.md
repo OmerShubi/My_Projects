802241ace3999becde9654353c1642a0633bab94
Omer S <omer.shubi@gmail.com>
2019-10-21 20:09:04 +0300

    Kappa and fista (#14)
    * added GD kappa effect & Image deblurring w/ FISTA
    
    * added GD kappa effect & Image deblurring w/ FISTA
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Rename Image_Related/Image Deblurring/generic_grad.m to Image_Related/Image_Deblurring/generic_grad.m
    
    * Rename Image_Related/Image Deblurring/fista.m to Image_Related/Image_Deblurring/fista.m
    
    * Rename Image_Related/Image Deblurring/exact_quad.m to Image_Related/Image_Deblurring/exact_quad.m
    
    * Rename Image_Related/Image Deblurring/const_step.m to Image_Related/Image_Deblurring/const_step.m
    
    * Rename Image_Related/Image Deblurring/blur.m to Image_Related/Image_Deblurring/blur.m
    
    * Rename Image_Related/Image Deblurring/README.md to Image_Related/Image_Deblurring/README.md
    
    * Rename Image_Related/Image Deblurring/GD_vs_FISTA_wrapper.m to Image_Related/Image_Deblurring/GD_vs_FISTA_wrapper.m
    
    * Rename

---
+++ Image_Related/Image_Deblurring/README.md
@@ -0,0 +1,3 @@
+[Results](/Image_Related/Image_Deblurring/doc/GD_vs_FISTA_wrapper.pdf)
+
+To reconstruct run [GD_vs_FISTA_wrapper.m](/Image_Related/Image_Deblurring/GD_vs_FISTA_wrapper.m)
---
+++ Image_Related/Image_Deblurring/generic_grad.m
@@ -0,0 +1,52 @@
+function [x ,x_1,x_10,x_100,x_1000] = generic_grad_q3(f , gf , lsearch , x0 , MAX_ITERATIONS)
+
+% Input checks
+if ~isa(f,'function_handle') || ~isa(gf,'function_handle') || ~isa(lsearch,'function_handle')
+	error("f, gf and lsearch  must be functions")
+end
+if ~isscalar(MAX_ITERATIONS) || ~(MAX_ITERATIONS==floor(MAX_ITERATIONS)) || ~isreal(MAX_ITERATIONS)
+    error("MAX_ITERATIONS must be a real scalar integer")
+end
+if ~isreal(x0)
+    error("x0 must be real")
+end
+
+% Initialize Paramaters
+x = x0;
+grad = gf(x);
+% Result containers
+
+iteration = 0;
+
+% Execute Generic Gradient Descent Algorithm
+% while (grad_norm > eps && iteration < MAX_ITERATIONS)
+while (iteration < MAX_ITERATIONS)
+
+    t_k = lsearch(f,x,grad);
+    x = x - (t_k * grad);
+    grad = gf(x);
+    
+    % Recording interim results
+    iteration=iteration+1;
+    if(iteration == 1)
+        x_1=x;
+        disp(['Iteration ', num2str(iteration), ' complete'])
+    end
+    if(iteration == 10)
+        x_10=x;
+        disp(['Iteration ', num2str(iteration), ' complete'])
+    end
+    if(iteration == 100)
+     x_100=x;
+    end
+    if(iteration == 1000)
+     x_1000=x;
+    end
+    
+    if(mod(iteration,100) == 0)
+        disp(['Iteration ', num2str(iteration), ' complete'])
+    end
+
+end
+end
+
